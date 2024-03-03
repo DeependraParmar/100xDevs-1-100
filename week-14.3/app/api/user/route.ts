0fce6d091576b9bd80aa3659dc7b97cb7d8fac20
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-03-03 20:35:00 +0530

    Nextjs 14.3 done

---
+++ week-14.3/app/api/user/route.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest } from "next/server";
+
+const client = new PrismaClient();
+
+export async function POST(req: NextRequest){
+    const body = await req.json();
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    })
+
+    console.log(user);
+    return Response.json({
+        message: 'You are logged in'
+    });
+}
